5b6024ff5db27ecc94d6511734bd455f05f94698
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-08-30 12:19:32 -0700

    reset button and counter

--- src/EightBall.js
+++ src/EightBall.js
@@ -22,18 +22,51 @@ function EightBall({ answers = ANSWERS }) {
     color: "black"
   });
 
+  const [count, setCount] = useState({
+    red: 0,
+    goldenrod: 0,
+    green: 0
+  });
 
-
+  /** handles click on Magic Eightball to display a new message and update 
+   * response counter */
   function handleClick() {
     let randIdx = getRandom(answers.length);
+    let color = answers[randIdx].color;
+
+    ++count[color];
+
     setResult(answers[randIdx]);
+    setCount(count);
+  }
+
+  /** handles click on reset button and resets message and color to default, 
+   * and response counters to 0 */
+  function resetClick() {
+    setResult({
+      msg: "Think of a Question.",
+      color: "black"
+    });
+    setCount({
+      red: 0,
+      goldenrod: 0,
+      green: 0
+    });
   }
 
   return (
-    <div className="EightBall"
-      onClick={handleClick}
-      style={{ backgroundColor: result.color }}>
-      <p className="msg"> {result.msg} </p>
+    <div className="game">
+      <div className="counter">
+        <p style={{color: "red"}}> Red: {count.red} |</p>
+        <p style={{color: "goldenrod"}}> Yellow: {count.goldenrod} |</p>
+        <p style={{color: "green"}}> Green: {count.green} </p>
+      </div>
+      <div className="EightBall"
+        onClick={handleClick}
+        style={{ backgroundColor: result.color }}>
+        <p className="msg"> {result.msg} </p>
+      </div>
+      <button onClick={resetClick}>Reset</button>
     </div>
   );
 }
